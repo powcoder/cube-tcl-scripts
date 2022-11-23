d4debf281d92aa919c19e4c19c5a9a6b44857218
powcoder <powcoder@163.com>
2022-11-23 12:37:43 +0800

    cube tcl scripts

---
+++ simple_spring.tcl
@@ -0,0 +1,18 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+system partSys dim 2
+system partSys particle 0 1 0 1 0 0 0 0 
+system partSys particle 1 1 1 1 0 0 0 0 
+
+simulator partSim link partSys 2
+simulator partSim spring 0 1 20 5 5
+
+#simulator partSim integration euler .001
+simulator partSim integration symplectic .01
+#simulator partSim integration verlet .001
+simulator partSim ground 300 50
+simulator partSim gravity 0
+
+
